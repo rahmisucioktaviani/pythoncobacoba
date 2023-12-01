b9bdca3f13f214bb00f414c374fefa87ba50231a
rahmisucioktaviani <151916624+rahmisucioktaviani@users.noreply.github.com>
2023-12-01 16:36:38 +0700

    lagi nyoba in guys maaf kalo salah

---
+++ perulanganbersarang.py
@@ -0,0 +1,15 @@
+for i in range(1,9) : 
+    print(i)
+    for j in range(1,11) :
+        print(i , "x" ,j, " = " , int(i*j))
+
+
+for i in range (1,6) :
+    print(i)
+    for j in range(1,6):
+        print(i , "/" ,j, " = " , int(i/j))
+for k in range(1,6):
+     print(k)  
+     for l in range(1,6) :
+        print(k , "+" , l , " = " , int(k+l))
+    
